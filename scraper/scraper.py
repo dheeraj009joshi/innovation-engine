e1f9ea4a03840c4bd81eda4dcb4eee159c80960d
Dheeraj Joshi <dlovej009@gmail.com>
2025-07-04 02:02:32 +0530

    commented transctipting the videos

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -106,13 +106,57 @@ class ScraperClient:
             print(err)
             pass
 
+
+
+    def get_hastag_posts_by_id_cursor(self,hastag_id, count):
+        out=[]
+        
+        cursor=0
+        while int(cursor) < count:
+
+            params = {
+            "id":hastag_id,
+            "cursor":cursor,
+            "access_key":self.token
+            } 
+            res=requests.get(f"{self.url}/hashtag/medias",params=params)
+            print(res.json())
+            if "itemList" in res.json():
+                cursor= res.json()["cursor"]
+                for item in res.json()["itemList"]:
+                    try:
+                        data={}
+                        data["description"]=item["desc"]
+                        data["id"]=item["id"]
+                        data["videoUrl"]=item["video"]["bitrateInfo"][0]["PlayAddr"]["UrlList"][-1]
+                        data["user_info"]={
+                            "name":item["author"]["nickname"],
+                            "bio":item["author"]["signature"],
+                            "username":item["author"]["uniqueId"],
+                            "followers":item["authorStats"]["followerCount"],
+                            "followings":item["authorStats"]["followingCount"],
+                            "totalLikes":item["authorStats"]["heart"]
+                        }
+                        data["createTime"]=datetime.fromtimestamp(item["createTime"]).isoformat()
+                        data["collectCount"]=item["stats"]["collectCount"]
+                        data["commentCount"]=item["stats"]["commentCount"]
+                        data["playCount"]=item["stats"]["playCount"]
+                        data["shareCount"]=item["stats"]["shareCount"]
+                        out.append(data)
+                        if res.json()["hasMore"]==False:
+                            break
+                    except:
+                        pass
+            print(len(out)) 
+        return out
+    
     
 aa=ScraperClient("1J3SttXjxlZIekKgvbX9sgyWtDQm8Zxh")
 
-# tt=aa.get_hastag_info("dove")
+# tt=aa.get_hastag_info("sleep")
 # print(tt)
-# res=aa.get_hastag_posts_by_id(1681086669362181,30,"")
-# # print(res)
+res=aa.get_hastag_posts_by_id_cursor(12563,10000)
+print(res)
 
 # res=aa.get_post_comments_by_post_id(7242381942933490971,100)
 # print(res)
